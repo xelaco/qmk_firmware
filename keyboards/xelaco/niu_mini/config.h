9a08a9d5bbff3b0da6b2b0a2e68209af7a5f6f80
xelaco <xelaco6@gmail.com>
2021-12-15 23:30:26 +0100

    Retrait mod tap + ALT GR sur SPACE gauche
    /!\ RGB en eeprom dorénavant
    Mise en forme du code et indentation

--- keyboards/xelaco/niu_mini/config.h
+++ keyboards/xelaco/niu_mini/config.h
@@ -67,6 +67,3 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
     #define RGBLIGHT_VAL_STEP 8
 #endif
 
-#define TAPPING_TERM 200
-#define TAPPING_TERM_PER_KEY
-#define IGNORE_MOD_TAP_INTERRUPT_PER_KEY
