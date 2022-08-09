97366651fb9a50df06d57963890822004e4255e3
xelaco <xelaco6@gmail.com>
2022-08-09 22:24:25 +0200

    Modification script édition, compil DB pour chaque clavier

--- xqmk.sh
+++ xqmk.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
-qmk generate-compilation-database -kb xelaco/pegasushoof -km xelaco
-qmk generate-compilation-database -kb xelaco/nanoslider -km xelaco
-qmk generate-compilation-database -kb xelaco/niu_mini -km xelaco
-vim -S qmk.vim
+cd keyboards/xelaco/pegasushoof/keymaps/xelaco
+qmk generate-compilation-database
+mv ../../../../../compile_commands.json .
+cd ../../../nanoslider/keymaps/xelaco/
+qmk generate-compilation-database
+mv ../../../../../compile_commands.json .
+cd ../../../niu_mini/keymaps/xelaco/
+qmk generate-compilation-database
+mv ../../../../../compile_commands.json .
+cd ../../../../..
+vim -p keyboards/xelaco/pegasushoof/keymaps/xelaco/keymap.c keyboards/xelaco/nanoslider/keymaps/xelaco/keymap.c keyboards/xelaco/niu_mini/keymaps/xelaco/keymap.c
